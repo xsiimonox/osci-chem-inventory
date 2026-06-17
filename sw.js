3a9aa4bcbcf1d0ac9a8988b232d2a63db8c9ee82
xsiimonox <46009617+xsiimonox@users.noreply.github.com>
2026-06-17 11:39:18 +0200

    Update sw.js

--- sw.js
+++ sw.js
@@ -1,5 +1,5 @@
-const CACHE_NAME = 'osci-lager-v1';
-const urlsToCache = [
+const CACHE_NAME = 'osci-v1';
+const ASSETS = [
   './',
   './index.html',
   './style.css',
@@ -7,18 +7,10 @@ const urlsToCache = [
   './manifest.json'
 ];
 
-self.addEventListener('install', event => {
-  event.waitUntil(
-    caches.open(CACHE_NAME)
-      .then(cache => cache.addAll(urlsToCache))
-  );
+self.addEventListener('install', (e) => {
+  e.waitUntil(caches.open(CACHE_NAME).then((cache) => cache.addAll(ASSETS)));
 });
 
-self.addEventListener('fetch', event => {
-  event.respondWith(
-    caches.match(event.request)
-      .then(response => {
-        return response || fetch(event.request);
-      })
-  );
+self.addEventListener('fetch', (e) => {
+  e.respondWith(caches.match(e.request).then((res) => res || fetch(e.request)));
 });
