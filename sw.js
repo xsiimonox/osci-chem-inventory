de9c6b7ff6ceee1528506e35b55c914fc3214758
xsiimonox <46009617+xsiimonox@users.noreply.github.com>
2026-06-17 11:45:54 +0200

    Update sw.js

--- sw.js
+++ sw.js
@@ -1,4 +1,4 @@
-const CACHE_NAME = 'osci-v1';
+const CACHE_NAME = 'osci-pwa-cache-v3';
 const ASSETS = [
   './',
   './index.html',
@@ -8,9 +8,13 @@ const ASSETS = [
 ];
 
 self.addEventListener('install', (e) => {
-  e.waitUntil(caches.open(CACHE_NAME).then((cache) => cache.addAll(ASSETS)));
+  e.waitUntil(
+    caches.open(CACHE_NAME).then((cache) => cache.addAll(ASSETS))
+  );
 });
 
 self.addEventListener('fetch', (e) => {
-  e.respondWith(caches.match(e.request).then((res) => res || fetch(e.request)));
+  e.respondWith(
+    caches.match(e.request).then((res) => res || fetch(e.request))
+  );
 });
