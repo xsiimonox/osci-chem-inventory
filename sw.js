1a389c6d95b14aacdf7fb3e85f19ac0a595f03e1
xsiimonox <46009617+xsiimonox@users.noreply.github.com>
2026-07-26 19:30:26 +0200

    Add files via upload

--- sw.js
+++ sw.js
@@ -1,4 +1,4 @@
-const CACHE_NAME = 'reef-storage-tools-cache-v312-nav-search-removed';
+const CACHE_NAME = 'reef-storage-tools-cache-v312-storage-crash-fix';
 const ASSETS = [
   './',
   './index.html',
